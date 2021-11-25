b99275e37f59a98391206d53080825b1f033ebe8
Bruna Topan <brutopanb@gmail.com>
2021-11-25 11:23:04 -0300

    Preparando review

--- src/server.js
+++
@@ -1,8 +0,0 @@
-import './setup.js';
-import app from './app.js';
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-  console.log(process.env.NODE_ENV);
-  console.log(process.env.DB_DATABASE);
-});
